abeeefaee2eb1ed696379289ec64c76a3fe2924a
Billy Kwok <xobkwok@gmail.com>
2015-06-29 13:09:48 +0800

    Add class SerialState

--- src/ts/serialState.ts
+++ src/ts/serialState.ts
@@ -1,23 +1,49 @@
 /// <reference path="tsd/bundle.d.ts" />
+/// <reference path="tsd/chrome.serial.d.ts" />
 /// <reference path="base.ts" />
 
 module RunningElderly {
-	export class SerialState {
+	function ab2str(buf: ArrayBuffer): string {
+		return String.fromCharCode.apply(null, new Uint8Array(buf));
+	}
 
-		constructor() {
-			var id: number = 0;
+	export class SerialState {
+		id: number;
+		devicePath: string;
+		strReceived: string;
+
+		constructor(onLineReceived: (str: string) => void) {
+			this.id = 0;
+			this.devicePath = '';
+			this.strReceived = '';
 
 			chrome.serial.getDevices((ports) => {
 				for (var i = 0; i < ports.length; i++) {
-					console.log(ports[i].path);
+					if (ports[i].path.indexOf('/dev/cu.usbmodem') > -1) this.devicePath = ports[i].path;
 				}
 			});
-			chrome.serial.connect(, { bitrate: 115200 }, (info: chrome.serial.ConnectionInfo) => {
+
+			chrome.serial.connect(this.devicePath, { bitrate: 115200 }, (info: chrome.serial.ConnectionInfo) => {
 				console.log(info);
-				id = info.connectionId;
+				this.id = info.connectionId;
+			});
+
+			chrome.serial.onReceive.addListener((info) => {
+				if (info.connectionId == this.id && info.data) {
+					var str = ab2str(info.data);
+					if (str.charAt(str.length - 1) === '\n') {
+						this.strReceived += str.substring(0, str.length - 1);
+						this._onLineReceived(onLineReceived);
+						this.strReceived = '';
+					} else {
+						this.strReceived += str;
+					}
+				}
 			});
 		}
 
-		static 
+		private _onLineReceived(callback: (str: string) => void) {
+			callback.apply(this, [ this.strReceived ]);
+		}
 	}
 }
